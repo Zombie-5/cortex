380484e3d958ca550a02b869041cba3473363f09
zombie <zombiewalkingdead5@gmail.com>
2025-04-11 10:45:32 +0100

    refactor

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -110,17 +110,17 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Deseja Terminar a sessão?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-body"> Clique em "Sair" se quiser terminar a sessão</div>
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
                     <form id="logout-form" action="{{ route('auth.signOut') }}" method="POST">
                         @csrf
-                        <button class="btn btn-primary type="submit">Logout</button>
+                        <button class="btn btn-primary type="submit">Sair</button>
                     </form>
                 </div>
             </div>
