1f95d8ad9f8ed37c3fe7ac4e8bf302e771e46674
zombie <zombiewalkingdead5@gmail.com>
2025-04-21 10:31:14 +0100

    updt

--- app/Http/Controllers/client/TeamController.php
+++ app/Http/Controllers/client/TeamController.php
@@ -12,8 +12,9 @@ class TeamController extends Controller
     public function generateInviteLink()
     {
         /* $baseURL = 'http://127.0.0.1:8000/sign-up';
-        $baseURL = 'http://localhost:8080/cortex/public/sign-up'; */
-        $baseURL = 'https://etoro.onrender.com/sign-up';
+        $baseURL = 'http://localhost:8080/cortex/public/sign-up'; 
+        $baseURL = 'https://etoro.onrender.com/sign-up'; */
+        $baseURL = config('app.url') . '/sign-up';
         $encodeId = Auth::user()->id;
         return $baseURL . '/' . $encodeId;
     }
